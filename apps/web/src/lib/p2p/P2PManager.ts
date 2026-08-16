fafbde7d406978297521ba2e687ff877fb780d0f
Rahul <rahul.vvs.jsr@gmail.com>
2026-08-16 21:56:50 +0530

    Add open TURN relay servers for mobile 4G/5G CGNAT traversal

--- apps/web/src/lib/p2p/P2PManager.ts
+++ apps/web/src/lib/p2p/P2PManager.ts
@@ -26,7 +26,17 @@ const PEER_CONFIG = {
       { urls: 'stun:stun3.l.google.com:19302' },
       { urls: 'stun:stun4.l.google.com:19302' },
       { urls: 'stun:global.stun.twilio.com:3478' },
+      {
+        urls: [
+          'turn:openrelay.metered.ca:80',
+          'turn:openrelay.metered.ca:443',
+          'turn:openrelay.metered.ca:443?transport=tcp',
+        ],
+        username: 'openrelay',
+        credential: 'openrelay',
+      },
     ],
+    iceCandidatePoolSize: 10,
   },
 };
 
